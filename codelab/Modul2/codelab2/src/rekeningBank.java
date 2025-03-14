4a73325631b7f4cc5b10498184f541ff0cb8f5b1
faza-pratama <alinnopratama@gmail.com>
2025-03-14 15:58:50 +0700

    menambah date API

--- codelab/Modul2/codelab2/src/rekeningBank.java
+++ codelab/Modul2/codelab2/src/rekeningBank.java
@@ -1,9 +1,16 @@
+import java.time.LocalDateTime; // Mengimpor kelas LocalDateTime dari package java.time. Kelas ini digunakan untuk merepresentasikan tanggal dan waktu.
+import  java.time.format.DateTimeFormatter; // Mengimpor kelas DateTimeFormatter dari package java.time.format. Kelas ini digunakan untuk memformat dan mengurai tanggal dan waktu.
+
 // Definisi kelas 'rekeningBank'
 public class rekeningBank {
     // Atribut kelas untuk menyimpan data rekening bank
     String nomorRekening; // Menyimpan nomor rekening
     String namaPemilik; // Menyimpan nama pemilik rekening
     double saldo; // Menyimpan saldo dalam rekening
+
+    LocalDateTime timenow = LocalDateTime.now();// Membuat objek LocalDateTime 'timenow' yang menyimpan tanggal dan waktu saat ini.
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Membuat objek DateTimeFormatter 'formatter' dengan pola format "yyyy-MM-dd".
+
 
     // Metode untuk menampilkan informasi rekening
     void tampilkanInfo() {
@@ -20,7 +27,7 @@ public class rekeningBank {
         // Menambahkan jumlah uang yang disetor ke saldo
         saldo += jumlah;
         // Menampilkan informasi setelah setoran
-        System.out.println(namaPemilik + " menyetor Rp." + jumlah + " saldo saat ini: Rp." + saldo + "\n");
+        System.out.println(namaPemilik + " menyetor Rp." + jumlah + " pada " + timenow.format(formatter) + " saldo saat ini: Rp." + saldo + "\n");
     }
 
     // Metode untuk menarik uang dari rekening
@@ -30,7 +37,7 @@ public class rekeningBank {
             // Mengurangi saldo dengan jumlah yang ditarik
             saldo -= jumlah;
             // Menampilkan informasi setelah penarikan
-            System.out.println(namaPemilik + " menarik Rp." + jumlah + " saldo saat ini: Rp." + saldo + "\n");
+            System.out.println(namaPemilik + " menarik Rp." + jumlah + " pada " + timenow.format(formatter) + " saldo saat ini: Rp." + saldo + "\n");
         } else {
             // Menampilkan pesan kesalahan jika saldo tidak mencukupi atau jumlah tidak valid
             System.out.println(namaPemilik + " menarik Rp." + jumlah + " (gagal saldo tidak mencukupi)" + " saldo saat ini: Rp." + saldo + "\n");
